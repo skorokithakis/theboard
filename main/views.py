5404ad88e6d0dff81f299557e2fbb7891d9c70ef
The Board LLM <llm@theboard>
2025-11-09 23:37:16 +0000

    Add retro feature buttons

--- main/views.py
+++ main/views.py
@@ -7,12 +7,19 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.decorators.http import require_GET
 
+from .models import Feature
 from .utils import get_next_iteration_at
 
 
 @require_GET
 def index(request: HttpRequest) -> HttpResponse:
+    button_features = (
+        Feature.objects.pending()
+        .only("id", "title", "description", "created_at")
+        .order_by("-created_at")
+    )
     context = {
         "next_iteration_at": get_next_iteration_at(),
+        "feature_buttons": button_features,
     }
     return render(request, "index.html", context)
